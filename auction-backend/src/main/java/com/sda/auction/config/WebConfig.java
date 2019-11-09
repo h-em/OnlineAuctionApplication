45cc63ef88c2daf860a10e7bcdba9fd7ee410145
emanuel <emy_halip@yahoo.com>
2019-11-09 12:34:43 +0200

    added frontend layer

---
+++ auction-backend/src/main/java/com/sda/auction/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sda.auction.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Halip on 09.11.2019.
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
---
+++ auction-frontend/resources/js/login.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $.get("http://localhost:8080/api/test", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+      });
+});
+
